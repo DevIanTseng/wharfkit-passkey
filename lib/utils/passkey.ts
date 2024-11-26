1212f8f8a6a6fc9a2d88bf1d1b5ed683c3de816c
DevIanTseng <iant.tseng@gmail.com>
2024-11-27 02:25:44 +0800

    log key

--- lib/utils/passkey.ts
+++ lib/utils/passkey.ts
@@ -88,10 +88,14 @@ export const decodeKey = async (payload: {
   ser.pushString(payload.rpid);
   const compact = ser.asUint8Array();
 
+  console.log(compact);
+
   const key = publicKeyToString({
     type: KeyType.wa,
     data: compact,
   });
+
+  console.log(key);
 
   return {
     credentialId: arrayToHex(credentialId),
